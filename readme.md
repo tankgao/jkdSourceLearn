85dd46103e8f94fca28f45712881a005b8174cde
gaochong <gaochong@and1gc-2.local>
2020-05-13 19:55:57 +0800

    初始化源码仓库

---
+++ readme.md
@@ -0,0 +1,8 @@
+# 需要先看的几个包
+- java.io
+- java.lang
+- java.math
+- java.net
+- java.nio
+- java.time
+- java.util
---
+++ src/cn/codesheep/test/hashMap/Test.java
@@ -0,0 +1,14 @@
+package cn.codesheep.test.hashMap;
+
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Test {
+    public static void main(String[] args) {
+        StringBuilder hello = new StringBuilder("hello");
+        System.out.println(hello);
+    }
+
+
+}
